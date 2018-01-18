931ec73efcfedc204887d1f96710db39087b30c1
Wei <wiwi571246@gmail.com>
2018-01-18 17:40:25 +0800

    Wei
    20180118

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @authors ${EKOISMYLOVE} (${elyismylove@gmail.com})
+ * @date    2018-01-18
+ * @version $Id$
+ */
+
+function test(){	
+	setTime();
+}
+
+function setTime(){
+	var T = new Date();
+	var hours = ('0'+ T.getHours()).substr(-2);
+	var minutes = ('0'+ T.getMinutes()).substr(-2);
+	var seconds = ('0'+ T.getSeconds()).substr(-2);
+
+	document.getElementById("time").innerHTML = "Now Time is : " + hours + ":" + minutes + ":" + seconds;	
+	var t=setTimeout("setTime()",1000);
+}
